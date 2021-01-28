e8fa054fba0bd3cbfe0c468f0936c62800bb2b54
Brandon Maroni <brandon-maroni@lambdastudents.com>
2021-01-27 18:16:16 -0700

    Created and completed PrivateRoute

--- front-end/src/utils/PrivateRoute.js
+++ front-end/src/utils/PrivateRoute.js
@@ -1,1 +1,18 @@
 import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+export function PrivateRoute(props) {
+  const { children, ...rest } = props;
+  return (
+    <Route
+      {...rest}
+      render={({location}) => {
+        return localStorage.getItem('token') ? (
+          children
+        ) : (
+          <Redirect to={{ pathname: '/login', state: { from: location}} } />
+        )
+      }}
+    />
+  )
+}
