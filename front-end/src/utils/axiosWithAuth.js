5f3fa485300e9fe40132c2f880ef042f4a82375e
Brandon Maroni <brandon-maroni@lambdastudents.com>
2021-01-27 18:35:57 -0700

    updated baseURL string

--- front-end/src/utils/axiosWithAuth.js
+++ front-end/src/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
   const token = localStorage.getItem('token');
 
   return axios.create({
-    baseURL: '',
+    baseURL: 'https://secret-fam-recipes.herokuapp.com/api',
     headers: { Authorization: token }
   });
 };
