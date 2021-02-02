cd50e73717b88b831f7576ac657104a8fcf11e20
Brandon Maroni <63432771+Bmmaroni@users.noreply.github.com>
2021-02-01 19:05:38 -0700

    restoring App to solve merge conflicts

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
-import UpdateRecipe from './components/UpdateRecipe';
 
 function App() {
   return (
-    <div className="App">
-      <UpdateRecipe />
-    </div>
+    <div className="App"></div>
   );
 }
 
